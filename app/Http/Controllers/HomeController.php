1148c0f0878f64b690ec689c00b1845a10fe8442
CV <https://github.com/geneangelo28/CV>
2022-12-01 13:50:51 +0800

    edit welcome

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
             return view('homepage', compact('orders', 'totals'));
        }
        else{
-            return view('homepage');
+            return view('welcome');
        }
     }
     
